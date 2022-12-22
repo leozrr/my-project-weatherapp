daa1e98afc53393bc6e46848c420630a2dce7b09
Leona <leona.zerr@gmail.com>
2022-12-22 19:38:43 +0100

    forecast added

--- src/script.js
+++ src/script.js
@@ -37,6 +37,38 @@ h2.innerHTML = `${month} ${date}`;
 let h3 = document.querySelector("h3");
 h3.innerHTML = `${day}, ${hour}:${minute}`;
 
+// Forecast
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 // Change city according to form input
 function city(event) {
   event.preventDefault();
